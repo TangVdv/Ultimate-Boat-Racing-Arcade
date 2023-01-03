d91aa60f9710493307fc8896d92dd036462d4a43
TangVdv <tangrider77120@gmail.com>
2023-01-03 12:38:47 +0100

    Complete cannon rotation view implementation

--- Assets/Scripts/CannonShootingScript.cs
+++ Assets/Scripts/CannonShootingScript.cs
@@ -17,6 +17,13 @@ public class CannonShootingScript : MonoBehaviour
         {
             for (int i = 0; i < barrels.Length; i++)
             {
+                var localScale = cannonBall.transform.localScale;
+                localScale = new Vector3(
+                    barrels[i].parent.lossyScale.x,
+                    barrels[i].parent.lossyScale.x,
+                    barrels[i].parent.lossyScale.z
+                );
+                cannonBall.transform.localScale = localScale;
                 GameObject bullet = Instantiate(cannonBall, barrels[i].position, barrels[i].rotation);
                 //bullet.GetComponent<Rigidbody>().velocity = barrels[i].forward * (force * Time.deltaTime);
                 bullet.GetComponent<Rigidbody>().AddForce(barrels[i].forward * force, ForceMode.Impulse);
--- Assets/Scripts/RotateCannonView.cs
+++ Assets/Scripts/RotateCannonView.cs
@@ -5,34 +5,59 @@ using UnityEngine;
 
 public class RotateCannonView : MonoBehaviour
 {
-    public GameObject bodyCannon;
-    public Camera cameraCannon;
+    [SerializeField] private Transform bodyCannon;
+    [SerializeField] private Transform cameraCannon;
 
-    public float rotationSideSpeed = .01f;
-    public float rotationUpDownSpeed = .01f;
+    private Vector3 _localRotation;
+    private float _cameraDistance = 6f;
+
+    public float mouseSensitivity = 10.0f;
+    public float rotationSideSpeed = 100f;
+    public float rotationUpDownSpeed = 50f;
+    public float orbitDampening = 10f;
 
     public float topClamp = 25.0f;
-    public float bottomClamp = .0f;
-    
-    // Update is called once per frame
-    private void LateUpdate()
+
+    private void Start()
     {
-        CameraRotation();
+        cameraCannon.position = bodyCannon.position;
+        cameraCannon.Translate(new Vector3(0, _cameraDistance, -_cameraDistance));
+        cameraCannon.Rotate(new Vector3(25, 0, 0));
     }
 
+    // Update is called once per frame
     private void Update()
     {
         BodyCannonRotation();
     }
 
+    // LateUpdate is called once per frame, after Update() on every game object in the scene
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    // Rotation of the camera on the X axis based on Mouse Coordinates
     private void CameraRotation()
     {
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            _localRotation.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+        }
         
+        Quaternion QT = Quaternion.Euler(0, _localRotation.x, 0);
+        cameraCannon.parent.rotation = Quaternion.Lerp(cameraCannon.parent.rotation, QT, Time.deltaTime * orbitDampening);
     }
 
     private void BodyCannonRotation()
     {
-        Debug.Log(bodyCannon.transform.localRotation.y);
-        Debug.Log(cameraCannon.transform.localRotation.y);
+        Quaternion targetRotation = Quaternion.Euler(0, _localRotation.x, 0);
+        Quaternion actualRotation = Quaternion.RotateTowards(bodyCannon.rotation, targetRotation, rotationSideSpeed * Time.deltaTime);
+
+        if (targetRotation != actualRotation)
+        {
+            Debug.Log("call");
+            bodyCannon.rotation = actualRotation;
+        }
     }
 }
